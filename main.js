7919702e21f097771a96b3257125c6cb8053fab2
GagoAnimate <65516039+gagominecraft12@users.noreply.github.com>
2020-09-19 22:53:24 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const Discord = require ('discord.js');
+
+
+
+const cilent = new Discord.Cilent();
+    console.log('A Bot Is Online!');
+});
+
+
+cilent.login('NzUzNjUzNzcxMzM0MTg5MTk2.X1pUxQ.BJzFn7NEekBCTCmAS5HQPnnzajI);
