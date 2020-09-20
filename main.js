09e66040ccb5982f320e037fbdcdfe7cb45d9826
GagoAnimate <65516039+gagominecraft12@users.noreply.github.com>
2020-09-19 22:57:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const Discord = require ('discord.js');
 
 
 
+const prefix = 'Bon_';
+
 const cilent = new Discord.Cilent();
     console.log('A Bot Is Online!');
 });
-
-
 cilent.login('NzUzNjUzNzcxMzM0MTg5MTk2.X1pUxQ.BJzFn7NEekBCTCmAS5HQPnnzajI);
